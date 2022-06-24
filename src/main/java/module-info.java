2d8623fb391cf9827bae434f1299271740ea01ce
Maciej Kosakowski <kosakowski222@gmail.com>
2022-06-24 18:37:48 +0200

    Punkt 4, Połączenie z bazą w Java.2

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module sample.lab7 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens pl.lublin.wsei.java.cwiczenia to javafx.fxml;
+    exports pl.lublin.wsei.java.cwiczenia;
+}
